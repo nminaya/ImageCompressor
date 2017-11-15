56ce770e5da721dbd94d974fb1330d0bb1c9f880
nminaya <nelsonminayaperez@gmail.com>
2017-11-15 07:12:17 -0400

    Adding ImageQuality comments

--- ImageCompressor/ImageQuality.cs
+++ ImageCompressor/ImageQuality.cs
@@ -2,10 +2,25 @@
 {
     public enum ImageQuality : short
     {
+        /// <summary>
+        /// Very Low Quality. Max Compress
+        /// </summary>
         VeryLow = 1,
+        /// <summary>
+        /// Low Quality
+        /// </summary>
         Low = 25,
+        /// <summary>
+        /// Medium Compress
+        /// </summary>
         Medium = 50,
+        /// <summary>
+        /// Intermediate Compress
+        /// </summary>
         Intermediate = 75,
+        /// <summary>
+        /// 0% Compressing
+        /// </summary>
         UnCompress = 100
     }
 }
