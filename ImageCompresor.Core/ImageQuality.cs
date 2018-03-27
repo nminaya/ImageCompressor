db4386aa2386e1d93986a389b170289ec87f0927
Nelson Minaya <nelson_minaya@claro.com.do>
2018-03-27 10:01:26 -0400

    Adding net standard project
    Co-Authored-By: Nelson Minaya  <nelsonminayaperez@gmail.com>

---
+++ ImageCompresor.Core/ImageQuality.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImageCompressor.Core
+{
+	/// <summary>
+	/// Imege Quality
+	/// </summary>
+	public enum ImageQuality : byte
+	{
+		/// <summary>
+		/// Very Low Quality. Max Compress
+		/// </summary>
+		VeryLow = 1,
+		/// <summary>
+		/// Low Quality
+		/// </summary>
+		Low = 25,
+		/// <summary>
+		/// Medium Compress
+		/// </summary>
+		Medium = 50,
+		/// <summary>
+		/// Intermediate Compress
+		/// </summary>
+		Intermediate = 75,
+		/// <summary>
+		/// 0% Compressing
+		/// </summary>
+		UnCompress = 100
+	}
+}
